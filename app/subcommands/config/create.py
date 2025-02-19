0bc77e8b0cde368297a22f20f86066fe2b49b84b
smierx <michel.dudas@googlemail.com>
2025-02-19 17:24:47 +0100

    added better validation of input and some module relevant params

--- app/subcommands/config/create.py
+++ app/subcommands/config/create.py
@@ -5,6 +5,17 @@ from app.schemas.modules import Module,Submodule
 
 app = typer.Typer()
 
+
+def get_valid_number(value: int,min_value:int, reason:str) -> int:
+    if value < min_value:
+        while True:
+            value = typer.prompt(f"Enter number of {reason}: ", type=int)
+            if value >= min_value:
+                return value
+            typer.echo(f"⚠️ Exception: The number has to be at least {min_value}!", err=True)
+    return value
+
+
 @app.command()
 def create(
     module: bool = typer.Option(False, "--module","-m", help="Create a new Module"),
@@ -13,6 +24,9 @@ def create(
     keydate: bool = typer.Option(False, "--keydate","-k", help="Create a new keydate"),
 
     name: str = typer.Argument(None,  help="Name of the module"),
+    number_of_submodules: int = typer.Option(None,"-n", help="Number of submodules"),
+    timestamp: str = typer.Option(None,"-t",help="Earliest relevant timestamp. Format=%Y-%m-%d"),
+    exam_format: bool = typer.Option(False,"-f",help="Exam format"),
 ):
     selected_options = [module, period,session, keydate]
     if sum(selected_options) != 1:
