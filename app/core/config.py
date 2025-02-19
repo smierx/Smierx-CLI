26c7082b97b7aea42f68ab62915e55fdee6948cc
smierx <michel.dudas@googlemail.com>
2025-02-19 17:20:03 +0100

    added version_view, base_path, module_path and submodules_path to env

--- app/core/config.py
+++ app/core/config.py
@@ -4,5 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env",env_file_encoding="utf-8")
     VERSION_CONFIG: str #TODO Automatisiert
+    VERSION_VIEW: str #TODO Automatisiert
+
+    BASE_PATH: str
+    MODULE_PATH: str
+    SUBMODULES_PATH: str
 
 settings = Config()
