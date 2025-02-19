47bc61bf74bc3d4fc1c848140cda9b0d178d7d12
smierx <michel.dudas@googlemail.com>
2025-02-19 12:57:15 +0100

    removed old ENV stuff

--- app/core/config.py
+++ app/core/config.py
@@ -3,14 +3,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Config(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env",env_file_encoding="utf-8")
-    BASE_PATH: str
-    ZETTEL_REL_PATH: str
-    LECTURE_REL_PATH: str
-    LECTURE_METADATA: str
-    LECTURE_METADATA_STR: str
-    TEMPLATE_REL_PATH: str
-    TEMPLATE_CSV: str
-    TABLES_REL_PATH: str
-    START_ROW: int = 4
+    VERSION_CONFIG: str #TODO Automatisiert
 
 settings = Config()
