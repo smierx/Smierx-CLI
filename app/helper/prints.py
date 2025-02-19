ad1e1a64732691f265837373afddd7f2b96903ed
smierx <michel.dudas@googlemail.com>
2025-02-19 17:19:13 +0100

    added table print

--- app/helper/prints.py
+++ app/helper/prints.py
@@ -4,3 +4,16 @@ from rich.table import Table
 import os
 def pprint(payload:any):
     rprint(payload)
+
+def pprint_table(payload:list[dict]):
+    table = Table(show_header=True, header_style="bold magenta",expand=True)
+
+    for column in payload[0].keys():
+        table.add_column(column, style="cyan",justify="center")
+
+    for row in payload:
+        table.add_row(*[str(row[col]) for col in row])
+
+    console = Console()
+    os.system("cls" if os.name == "nt" else "clear")
+    console.print(table)
